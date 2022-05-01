cba7f98d60bef4439d5ebd1363f1de4a85ae206e
mostofa kamal <mostofakamal1990@gmail.com>
2022-05-02 04:31:05 +0600

    car warehouse install and basic bootstraping

---
+++ firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import{getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFyu-Gy2GNxuE8APnEEsY40h9x_h7-T54",
+  authDomain: "car-warehouse-client.firebaseapp.com",
+  projectId: "car-warehouse-client",
+  storageBucket: "car-warehouse-client.appspot.com",
+  messagingSenderId: "483644367717",
+  appId: "1:483644367717:web:5d13f3b52cf6aa2af1b78c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
