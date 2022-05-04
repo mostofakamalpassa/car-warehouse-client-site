8c2795aa18bc4446ce0a56e1ee690a6ea52f1321
mostofa kamal <mostofakamal1990@gmail.com>
2022-05-04 12:33:48 +0600

    react route initilize

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+import Blog from './Pages/Blog/Blog';
+import About from './Pages/About/About';
+
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/about' element={<About></About>}></Route>
+       <Route path='/blog' element={<Blog></Blog>}></Route>
+     </Routes>
+  
+      <Header></Header>
+      <Home></Home>
       <Register></Register>
       <Login></Login>
     </>
