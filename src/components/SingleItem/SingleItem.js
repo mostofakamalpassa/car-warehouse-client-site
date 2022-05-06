95cc66ea99ae37b070197139992a5dc89873eedf
mostofa kamal <mostofakamal1990@gmail.com>
2022-05-06 23:24:26 +0600

    single item show data

--- src/components/SingleItem/SingleItem.js
+++ src/components/SingleItem/SingleItem.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const SingleItem = (props) => {
     console.log(props);
+    const {productName, price, qty, imageUrl,supplier,productDescription} = props.product;
     return (
-        
-       
           <Col>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+            <Card className="shadow-lg" style={{"borderTop":"5px solid red"}}>
+              <Card.Img variant="top" src={imageUrl} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title className='bg-dark p-2 text-info'>{productName}</Card.Title>
                 <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit longer.
+                 {productDescription}
                 </Card.Text>
+                <Card.Subtitle >Price = {price}$ <span>Qty = {qty}</span></Card.Subtitle>
+                <Card.Subtitle className="pt-2">Supplier = {supplier}</Card.Subtitle>
+                <Button className='mt-3 mx-auto w-100 fw-bold'><Link to='/inventory/' className='text-light text-decoration-none'>Update</Link></Button>
               </Card.Body>
             </Card>
-          </Col>
-        
-     
+          </Col> 
     );
 };
 
